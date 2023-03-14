e2d9e2eb5a6af75ff559c452afff3a72b61fc506
Fhellype Vinicius de Paula <fhellypeviniciusdepaula96@gmail.com>
2023-03-13 23:49:17 -0300

    dinamica

---
+++ script.js
@@ -0,0 +1,11 @@
+function menu() {
+    var body = document.querySelector('body');
+    var menu = document.querySelector('div#menu');
+    menu.style.display = 'block'
+}
+
+function fechar() {
+    var body = document.querySelector('body');
+    var menu = document.querySelector('div#menu');
+    menu.style.display = 'none'
+}
