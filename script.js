5a2ab199c2dffdc0c64d3ee7a05871bcddc358b6
Fhellype Vinicius de Paula <fhellypeviniciusdepaula96@gmail.com>
2023-03-14 22:45:02 -0300

    .

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 function menu() {
-    var body = document.querySelector('body');
     var menu = document.querySelector('div#menu');
+    var fundo = document.querySelector('div#azul');
+    fundo.style.display = 'none'
     menu.style.display = 'block'
 }
 
 function fechar() {
     var body = document.querySelector('body');
     var menu = document.querySelector('div#menu');
+    var fundo = document.querySelector('div#azul');
+    fundo.style.display = 'block'
     menu.style.display = 'none'
 }
 
 function carrinho() {
     var menu = document.querySelector('div#menu');
     var carrinho = document.querySelector('div#carrinho');
+    var fundo = document.querySelector('div#azul');
+    fundo.style.display = 'none'
     carrinho.style.display = 'block'
-    menu.style.display = 'none'
+    
 }
 
 function fcarrinho() {
     var carrinho = document.querySelector('div#carrinho');
+    var fundo = document.querySelector('div#azul');
+    fundo.style.display = 'block'
     carrinho.style.display = 'none'
 }
