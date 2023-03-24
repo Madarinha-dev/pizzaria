c1637b6b126df479b3696b53063ff81cf36d73a2
Fhellype Vinicius de Paula <fhellypeviniciusdepaula96@gmail.com>
2023-03-24 20:58:10 -0300

    oi

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 
-function abrir() {
-    var icone = document.querySelector('div.bloco-3');
-    icone.style.display = 'none';
-
+function abrir_index() {
     var menu = document.querySelector('div#menu');
     menu.style.display = 'block';
 
@@ -10,58 +7,54 @@ function abrir() {
     fundo.style.display = 'none';
 }
 
-
-function fechar0() {
-    var icone = document.querySelector('div.bloco-3');
-    icone.style.display = 'block';
-
+function fechar_x_index() {
     var menu = document.querySelector('div#menu');
+    var fundo = document.querySelector('div#azul');
+    fundo.style.display = 'block';
     menu.style.display = 'none';
 }
 
-
-function fechar() {
-    var menu = document.querySelector('div#menu');
-    var fundo = document.querySelector('div#azul');
-    fundo.style.display = 'block'
-    menu.style.display = 'none'
-}
-
-
-function carrinho0() {
-    var menu = document.querySelector('div#menu');
-    menu.style.display = 'none';
-
+function carrinho_index() {
     var carrinho = document.querySelector('div#carrinho');
-    carrinho.style.display = 'block';
-
-    var icones = document.querySelector('div.icones');
-    icones.style.display = 'none';
-}
-
-function fecharcarrinho() {
-    var icones = document.querySelector('div.icones');
-    icones.style.display = 'block';
-
-    var carrinho = document.querySelector('div#carrinho');
-    carrinho.style.display = 'none';
-}
-
-function carrinho() {
-    var carrinho = document.querySelector('div#carrinho');
-    var fundo = document.querySelector('div#azul');
-    var menu = document.querySelector('div#menu');
-
+    var fundo = document.querySelector('div#azul')
+    var menu = document.querySelector('div#menu')
     menu.style.display = 'none'
     fundo.style.display = 'none'
     carrinho.style.display = 'block'
-    
 }
 
 
-function fc() {
+function fechar_carrinho_index() {
     var carrinho = document.querySelector('div#carrinho');
     var fundo = document.querySelector('div#azul');
-    fundo.style.display = 'block'
-    carrinho.style.display = 'none'
+    fundo.style.display = 'block';
+    carrinho.style.display = 'none';
+}
+
+function abrir_aba_comecar_pedido() {
+    var barra = document.querySelector('div#menu');
+    var fundo = document.querySelector('div.bloco-3');
+    fundo.style.display = 'none';
+    barra.style.display = 'block';
+}
+
+function fechar_aba_comecar_pedido() {
+    var barra = document.querySelector('div#menu');
+    var fundo = document.querySelector('div.bloco-3');
+    fundo.style.display = 'block';
+    barra.style.display = 'none';
+}
+
+function abrir_carrinho_comecar_pedido() {
+    var carrinho = document.querySelector('div#carrinho');
+    var fundo = document.querySelector('div.icones')
+    carrinho.style.display = 'block';
+    fundo.style.display = 'none';
+}
+
+function fechar_carrinho_comecar_pedido() {
+    var carrinho = document.querySelector('div#carrinho');
+    var fundo = document.querySelector('div.icones');
+    carrinho.style.display = 'none';
+    fundo.style.display = 'block';
 }
